6f625a813d6ff707374ed593e0ef70546acc8a45
Daniel Fonseca Reis <daniel_fonseca_reis@hotmail.com>
2017-03-21 17:38:35 -0300

    versão inicial - realm

---
+++ app/src/main/java/danieeelfr/projects/android/mypoints/Business/PointBusiness.java
@@ -0,0 +1,8 @@
+package danieeelfr.projects.android.mypoints.Business;
+
+/**
+ * Created by danielfr on 21/03/2017.
+ */
+
+public class PointBusiness {
+}
---
+++ app/src/main/java/danieeelfr/projects/android/mypoints/Models/PointModel.java
@@ -0,0 +1,8 @@
+package danieeelfr.projects.android.mypoints.Models;
+
+/**
+ * Created by danielfr on 21/03/2017.
+ */
+
+public class PointModel {
+}
